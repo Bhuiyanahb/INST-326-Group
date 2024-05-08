541ba8aecfdbe92f782ad997dd31a505294bc422
Bhuiyanahb <157722961+Bhuiyanahb@users.noreply.github.com>
2024-05-08 12:52:51 -0400

    Update Unit_Tests.py

--- Unit_Tests.py
+++ Unit_Tests.py
@@ -34,11 +34,23 @@ class TestRecipeBook(unittest.TestCase):
         
         
     def test_list_recipes(self): #Arafat, 1st Unit Test
-        
+          # Create a RecipeBook object
+        recipe_book = RecipeBook(self.filename)
+        # Test listing recipes
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            recipe_book.list_recipes()
+            output = fake_out.getvalue().strip()
+            self.assertIn("Recipes:", output)  # Check if recipes are listed
         
     def test_edit_recipe(self): #Arafat, 2nd Unit Test
-        
-        
+         # Create a RecipeBook object
+        recipe_book = RecipeBook(self.filename)
+        # Test editing an existing recipe
+        with patch('builtins.input', side_effect=["yes", "Cheese, Pepperoni"]):
+            recipe_book.edit_recipe("Pasta")
+            self.assertEqual([ingredient.strip() for ingredient in recipe_book.recipes[0].ingredients], ["Cheese", "Pepperoni"])
+
+
     def test_search_recipe_book(self): #Dimitri, Only Unit Test
         # Create a RecipeBook object
         recipe_book = RecipeBook(self.filename)
